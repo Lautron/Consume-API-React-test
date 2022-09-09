14832a53f3c78d867eba6c53e3c8d303072d20fe
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-09 10:08:44 -0300

    Started used controlled components and replaced ternary operator with &&

--- src/components/SpotifySearchbar.jsx
+++ src/components/SpotifySearchbar.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 let SpotifySearchbar = (props) => {
-  return !props.display ? (
-    ""
-  ) : (
-    <div>
-      <input style={{ width: "500px" }} id="myInput" type="text" />
-      <input
-        onClick={() => {
-          props.handler(document.getElementById("myInput").value);
-        }}
-        type="submit"
-      />
-    </div>
+  const [query, setQuery] = useState("");
+
+  return (
+    props.display && (
+      <div>
+        <input
+          style={{ width: "500px" }}
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <input onClick={() => props.handler(query)} type="submit" />
+      </div>
+    )
   );
 };
 export default SpotifySearchbar;
