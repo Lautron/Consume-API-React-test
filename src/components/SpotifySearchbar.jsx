866b9b02f76cbcaeeb403f9ce4002a547b898555
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-09 12:33:26 -0300

    Added throttle function and created SearchResult component

--- src/components/SpotifySearchbar.jsx
+++ src/components/SpotifySearchbar.jsx
@@ -1,4 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getSpotifySearchResults } from "../api";
+import throttle from "lodash/throttle";
+
+const throttledSearchAPI = throttle(
+  async (query) => {
+    return await getSpotifySearchResults(query);
+  },
+  3000,
+  { leading: true }
+);
+
+const SearchResult = (props) => {
+  return (
+    <div onClick={() => props.onClickHandler(props.name, props.artists[0])}>
+      <img src={props.img_url} alt={`${props.name} album cover`} />
+      <strong>{props.name}</strong> by {props.artists.join(", ")}
+    </div>
+  );
+};
 
 let SpotifySearchbar = (props) => {
   const [query, setQuery] = useState("");
