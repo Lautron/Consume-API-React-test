847343aaf0287abeccc53a83963d9d9cf8b89030
Lautaro Bachmann <laurigbach@gmail.com>
2022-09-15 11:22:32 -0300

    Started handling verse ordering with a priority queue * Added CONFIG object to easily manage behaviour * Created a class to encapsulate external priority queue library * created usePriorityQueue hook

--- src/components/Lyrics.jsx
+++ src/components/Lyrics.jsx
@@ -1,4 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import FlatQueue from "flatqueue";
+
+const CONFIG = {
+  priorityChange: {
+    Again: -5,
+    Hard: -1,
+    Good: 0,
+    Easy: 5,
+  },
+  defaultDifficulty: 0,
+};
+
+CONFIG.difficulties = Object.keys(CONFIG.priorityChange);
 
 const StyledButton = (props) => {
   return (
@@ -16,13 +29,16 @@ let Verse = (props) => {
     <StyledButton clickHandler={handleClick} text="Show translation" />
   );
 
-  const difficulties = ["Hard", "Good", "Easy"];
   const flashcardSideB = (
     <>
       <span>{props.trans}</span>
       <br />
-      {difficulties.map((difficulty) => (
-        <StyledButton clickHandler={props.reviewDifficulty} text={difficulty} />
+      {CONFIG.difficulties.map((difficulty) => (
+        <StyledButton
+          clickHandler={() => props.handleDifficulty(difficulty)}
+          text={difficulty}
+          key={difficulty}
+        />
       ))}
     </>
   );
@@ -37,15 +53,69 @@ let Verse = (props) => {
   );
 };
 
+class PqueueWrapper {
+  constructor(array) {
+    this.pqueue = new FlatQueue();
+    array.forEach((_, index) =>
+      this.pqueue.push(index, CONFIG.defaultDifficulty)
+    );
+  }
+
+  pop() {
+    return this.pqueue.pop();
+  }
+
+  peek() {
+    return this.pqueue.peek();
+  }
+
+  peekPriority() {
+    return this.pqueue.peekValue();
+  }
+
+  push(item, priority) {
+    this.pqueue.push(item, priority);
+  }
+}
+
+const usePriorityQueue = (array) => {
+  const pqueue = useMemo(() => new PqueueWrapper(array), [array]);
+  let [current, setCurrent] = useState(0);
+
+  const pushBackAndSetNew = (priority) => {
+    const newIndex = pqueue.pop();
+    pqueue.push(newIndex, priority);
+    setCurrent(newIndex);
+    console.log(`newIndex: ${newIndex}`);
+  };
+
+  const dispatch = (difficulty) => {
+    const oldPriority = pqueue.peekPriority();
+    const newPriority = oldPriority + CONFIG.priorityChange[difficulty];
+    console.log(
+      `oldPriority: ${oldPriority}, newPriority: ${newPriority}, difficulty: ${difficulty}`
+    );
+    if (newPriority > 10) {
+      pqueue.pop();
+    }
+    pushBackAndSetNew(newPriority);
+  };
+
+  return [current, dispatch];
+};
+
 let Lyrics = (props) => {
-  let [index, setIndex] = useState(0);
+  let [index, dispatchDifficulty] = usePriorityQueue(props.songData);
+  if (props.songData) {
+    props.songData[index] && console.log(index, props.songData[index][0]);
+  }
   let verseArr = !props.songData
     ? ["Cargando"]
     : props.songData.map((verse, index) => {
         return (
           <Verse
             key={index}
-            reviewDifficulty={() => setIndex(index + 1)}
+            handleDifficulty={dispatchDifficulty}
             original={verse[0]}
             trans={verse[1]}
           />
